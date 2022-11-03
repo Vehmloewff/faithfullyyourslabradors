24b6075e280aedde9a9e776d90d7994ead1a799d
Elijah Mooring <vehmloewff@gmail.com>
2022-11-02 22:03:22 -0700

    ci: require more envs in pm2 script

--- devops/pm2-envs.js
+++ devops/pm2-envs.js
@@ -4,8 +4,8 @@ require('dotenv').config()
 /* Used by pm2-start/stop to check for required .env values */
 
 // Check for required ENVs.
-const { PORT, NAME, NODE_ENV } = process.env
-if (!PORT || !NAME || !NODE_ENV) {
+const { PORT, NAME, NODE_ENV, MONGO_URI, SESSION_SECRET } = process.env
+if ((!PORT || !NAME || !NODE_ENV || MONGO_URI, !SESSION_SECRET)) {
   console.log('\nPlease be sure your .env file includes values for PORT, NAME, and NODE_ENV.')
   process.exit()
 }
@@ -13,4 +13,4 @@ if (!PORT || !NAME || !NODE_ENV) {
 // Combine name and environment to identify the app instance. e.g., Flightplan - Production
 const INSTANCE = `${NAME} - ${NODE_ENV}`
 
-module.exports = { PORT, INSTANCE, NODE_ENV }
+module.exports = { PORT, INSTANCE, NODE_ENV, SESSION_SECRET, MONGO_URI }
