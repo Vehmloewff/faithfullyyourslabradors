6b407b7e6323693826b2d5cd6a54a283bba5969e
Elijah Mooring <vehmloewff@gmail.com>
2022-12-03 10:51:17 -0600

    feat: deploy special

--- CurrentSpecial.tsx
+++ CurrentSpecial.tsx
@@ -2,28 +2,18 @@ import { h } from './deps.ts'
 
 export function CurrentSpecial() {
 	return (
-		<div class="p-20 flex flex-col gap-20 bg-[#fff]">
-			<div class="flex gap-10 items-center">
-				<img src="/santa-hat.jpeg" alt="Santa Hat" class="w-50 h-50" />
-				<h3 class="text-2xl text-dark">Christmas Puppies</h3>
+		<div>
+			<div class="p-20 flex flex-col gap-20 bg-[#fff]">
+				<div class="flex gap-10 items-center">
+					<img src="/santa-hat.jpeg" alt="Santa Hat" class="w-50 h-50" />
+					<h3 class="text-2xl text-dark">Christmas Puppies</h3>
+				</div>
+
+				<p class="text-dark">New litter of Christmas puppies just born! Call our text us for more information.</p>
 			</div>
 
-			{/* <p class="text-dark">Treat a loved one to a special Christmas gift!</p> */}
-			<p class="text-dark">
-				Christmas puppies ready to go home the first week of December. These three little ladies are still available. Text or call
-				us for more information!!
-			</p>
-
 			<div class="flex gap-10 flex-col md:flex-row">
-				<div>
-					<img src="/christmas-pup1.png" alt="Christmas Puppy" />
-				</div>
-				<div>
-					<img src="/christmas-pup2.png" alt="Christmas Puppy" />
-				</div>
-				<div>
-					<img src="/christmas-pup3.png" alt="Christmas Puppy" />
-				</div>
+				<img src="/puppies-on-bench.png" alt="Christmas Puppies" />
 			</div>
 		</div>
 	)
